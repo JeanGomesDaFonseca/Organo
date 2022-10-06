c2a31c8af7651ede6dc8afd4bb71603531dd742d
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-10-06 18:47:02 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-vindo Jean, ao curso de React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+
     </div>
   );
 }
